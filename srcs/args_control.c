8d19907701c9164c5dfd16e1caf8c1778ee6c0fe
Aung Phyo Htet <aungphyohtet1@gmail.com>
2025-12-14 22:51:59 +0700

    arg_control. construct the stack A from the program arguments. need to do the arg checks which means to accept only the valid values only.

--- srcs/args_control.c
+++ srcs/args_control.c
@@ -1,24 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   args_control.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aphyo-ht <aphyo-ht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/14 22:34:07 by aphyo-ht          #+#    #+#             */
-/*   Updated: 2025/12/14 22:51:08 by aphyo-ht         ###   ########.fr       */
+/*   Created: 2025/12/14 22:33:18 by aphyo-ht          #+#    #+#             */
+/*   Updated: 2025/12/14 22:48:28 by aphyo-ht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "push_swap.h"
 
-int main(int argc, char **argv)
-{
+// TOdo check errors when the input is not integer or exceed the limits.
+
+t_stack *get_stack_from_args(char **argv)
+{	
 	t_stack *stack_a;
-
-	stack_a = get_stack_from_args(argv+1);
-
-	print_stack(stack_a, "Stack A");
+	t_node	*node;
 	
+	stack_a = get_stack();
+	while(*argv)
+	{
+		node = get_node(ft_atoi(*argv));
+		push_stack(stack_a, node);
+		argv++;
+	}
+	return stack_a;
 }
--- srcs/main_old.c
+++ srcs/main_old.c
@@ -5,20 +5,40 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aphyo-ht <aphyo-ht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/14 22:34:07 by aphyo-ht          #+#    #+#             */
-/*   Updated: 2025/12/14 22:51:08 by aphyo-ht         ###   ########.fr       */
+/*   Created: 2025/12/12 12:26:51 by aphyo-ht          #+#    #+#             */
+/*   Updated: 2025/12/13 17:41:28 by aphyo-ht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "push_swap.h"
-
+#include <stdio.h>
 int main(int argc, char **argv)
 {
+
 	t_stack *stack_a;
+	t_stack	*stack_b;
+	t_node	*node;
+	int		i;
 
-	stack_a = get_stack_from_args(argv+1);
-
+	int arr[] = {1,2,3,4,5,6};
+	stack_a = init_stack(arr, 6);
+	stack_b = get_stack();
 	print_stack(stack_a, "Stack A");
-	
+	print_stack(stack_b, "Stack B");
+	swap(stack_a, "sa");
+	pop_push(stack_a,stack_b,"b");
+	print_stack(stack_a, "Stack A");
+	print_stack(stack_b, "Stack B");
+
+	// head_to_tail(stack);
+	// tail_to_head(stack);
+	// ft_printf("asdfasdf");
+	// rotate(stack, "ra");
+	// head_to_tail(stack);
+	// tail_to_head(stack);
+	// reverse_rotate(stack, "rra");
+	// head_to_tail(stack);
+	// tail_to_head(stack);
+
+	return (0);
 }
